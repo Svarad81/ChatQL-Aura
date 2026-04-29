b1974736778573e350acda11ed8be5980f89a1bf
Varad Chakrawar <svaradchakrawar@gmail.com>
2026-04-30 00:29:38 +0530

    debug: add version flag

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+window.CHATQL_VERSION = 'AURA_V8_DECOUPLED';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
